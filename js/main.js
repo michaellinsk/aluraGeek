337d7a3c5a1d4d010634227bd936d92dbd6f904b
michaellinsk <michael.linsk01@gmail.com>
2024-11-18 21:32:58 -0300

    codigos

---
+++ js/main.js
@@ -0,0 +1,47 @@
+async function fetchProducts() {
+  try {
+    const response = await fetch("http://localhost:3000/products");
+    const data = await response.json();
+    handleFetchedProducts(data);
+  } catch (error) {
+    console.error("Erro:", error.message);
+  }
+}
+
+function handleFetchedProducts(products) {
+  const cardContainer = document.getElementById("cardContainer");
+
+  if (products.length < 1) {
+    cardContainer.innerHTML = `
+ <div  class="found">
+ <img src="./img/image.png" alt="imagem carinha triste">
+  <h1>
+  Você não tem nenhum produto
+  <h1>
+ </div>
+  `;
+    return;
+  }
+
+  products.forEach((product) => {
+    const newDiv = document.createElement("div");
+
+    newDiv.innerHTML = `
+        <div class="card">
+          <div><img src="${product.image}" alt="${product.title}" /></div>
+          <p class="card-title">${product.title}</p>
+          <div class="price">
+            <p>$ ${product.price.toFixed(2)}</p>
+            <span class="clear"  
+            }"><i class="bi bi-trash3-fill" id="${
+              product.id
+            }"  onclick="handleClick(event)"></i></span>
+          </div>
+        </div>
+      `;
+
+    cardContainer.appendChild(newDiv);
+  });
+}
+
+fetchProducts();
